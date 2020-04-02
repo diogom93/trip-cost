cb1d34aedbe94519c301c5ef18d874d853dfd094
Diogo Martins <nunodiogomartins@gmail.com>
2020-04-02 21:05:13 +0100

    Add API stubs

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongo = require('mongodb');
+
+const app = express();
+
+app.get('/trips', (req, res) => {
+
+});
+
+app.post('/trips', (req, res) => {
+
+});
+
+app.get('/expenses', (req, res) => {
+
+});
+
+app.post('/expenses', (req, res) => {
+
+});
+
+app.listen(3000, () => console.log('Server up and ready!'));
