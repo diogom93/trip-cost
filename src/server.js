7ee3235d374bddd56b4d13b7681daf4b7c734d92
Diogo Martins <nunodiogomartins@gmail.com>
2020-04-05 19:57:29 +0100

    Create endpoint to fetch trips

--- src/server.js
+++ src/server.js
@@ -19,7 +19,15 @@ mongo.connect(url, (err, client) => {
 app.use(express.json());
 
 app.get('/trips', (req, res) => {
-
+  trips.find().toArray((err, result) => {
+    if (err) {
+      console.log(error);
+      res.status(500).json({ok: false});
+      return;
+    }
+    console.log(result);
+    res.status(200).json({trips: result});
+  });
 });
 
 app.post('/trips', (req, res) => {
